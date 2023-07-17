bf7959b5e1f906a3b1336a39b993f58a458bc018
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-07-17 16:35:13 +0900

    FEAT : App component 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Profile from "./components/Profile";
+import Input from "./components/Input";
+import {ChangeEvent, useState} from 'react';
+import fetchUser from "./userSearchService";
+import * as S from './styled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [value, setValue] = useState<string>('');
+    const [profileName, setprofileName] = useState<string>('');
+    const [profileImg, setprofileImg] = useState<string>('');
+    const [profileFollowers, setprofileFollowers] = useState<number>(0);
+    const [profileFollowing, setprofileFollowing] = useState<number>(0);
+    const [profileUrl, setprofileUrl] = useState<string>('');
+
+    const handleInput = ({target: {value}}: ChangeEvent<HTMLInputElement>) => {
+        setValue(value);
+    }
+
+    const handleKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            const data = await fetchUser(value);
+            if (data.message === 'Not Found') {
+                alert('Not Found');
+                return;
+            }
+            setprofileName(data.name || data.login);
+            setprofileImg(data.avatar_url);
+            setprofileFollowers(data.followers);
+            setprofileFollowing(data.following);
+            setprofileUrl(data.html_url);
+        }
+    };
+
+    return (
+        <S.card>
+            <Input value={value} onChange={handleInput} onKeyUp={handleKeyPress} placeholder="user search"/>
+            {profileName && <Profile src={profileImg} name={profileName} followers={profileFollowers} following={profileFollowing} url={profileUrl}/>}
+        </S.card>
+    );
 }
-
-export default App;
---
+++ src/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  width: 300px;
+  height: 250px;
+  border-radius: 5px;
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+  background: #fff;
+`;
