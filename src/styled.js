302f71cb9ff5413ec59d1669150c5b5449a8fa89
jyoukwon <126861324+youkwon515@users.noreply.github.com>
2023-07-18 14:04:04 +0900

    FEAT : change App.js style

--- src/styled.js
+++ src/styled.js
@@ -5,8 +5,8 @@ export const card = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 10px;
-  width: 300px;
-  height: 250px;
+  width: 600px;
+  height: 550px;
   border-radius: 5px;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
   background: #fff;
